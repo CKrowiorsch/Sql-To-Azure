bef484859a8fb518e504a3fe9d20735fd4017fa8
Christian Krowiorsch <christian.krowiorsch@googlemail.com>
2019-07-19 15:28:51 +0200

    Projekt Setup

---
+++ Source/SqlToAzure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlToAzure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
