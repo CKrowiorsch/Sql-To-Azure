50d67c46d7575fb196a856a0cd4c20ab3d3be66e
ckrowiorsch <christian.krowiorsch@googlemail.com>
2020-06-08 15:03:11 +0200

    add settingsfile

---
+++ Source/SqlToAzure/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Krowiorsch
+{
+    public class Settings
+    {
+        public string Identifier { get; set; }
+        public string AzureConnection { get; set; }
+
+        public string AzureTableName { get; set; }
+
+        public string SqlServerConnection { get; set; }
+        public string SqlTableName { get; set; }
+        public string IdColumn { get; set; }
+
+        public string TimestampColumn { get; set; } = "Timestamp";
+
+
+        public static Settings ReadFromFile(string fileInfo)
+        {
+            return JsonConvert.DeserializeObject<Settings>(File.ReadAllText(fileInfo));
+        }
+
+    }
+}
